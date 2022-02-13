2f738f4b553011d71512c35f5122dc5f38c0aa97
Andrey-Pank <99199092+Andrey-Pank@users.noreply.github.com>
2022-02-13 19:15:53 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,37 +1,37 @@
 int gcd(int a, int b){
 	int del = 0;
-	if(a == b){
+	if(a == b) {
 		return a;
 	}
-	else if(a < b){
-		if(0 == (b % a)){
+	else if(a < b) {
+		if(0 == (b % a)) {
 			return a;
 		}
-		else{
+		else {
 			del = a;
-			for(; 0 < del; del--){
-				if(0 == (a%del) && 0 == (b%del)){
+			for(; 0 < del; del--) {
+				if(0 == (a%del) && 0 == (b%del)) {
 					return del;
 					break;
 				}
 			}
 		}
 	}
-	else{
-		if(0 == (a % b)){
+	else {
+		if(0 == (a % b)) {
 			return b;
 		}
 		else{
 			int del = b;
-			for(; 0 < del; del--){
-				if(0 == (b%del) && 0 == (a%del)){
+			for(; 0 < del; del--) {
+				if(0 == (b%del) && 0 == (a%del)) {
 					return del;
 					break;
 				}
 			}
 		}
 	}
-	if(0 == del){
+	if(0 == del) {
 		return 1;
 	}
 	return 0;
