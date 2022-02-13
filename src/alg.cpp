1421ac97e7a3f33512fc5543899afc854f3e0e1d
Andrey-Pank <99199092+Andrey-Pank@users.noreply.github.com>
2022-02-13 16:23:37 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,5 +1,39 @@
 // Copyright 2021 NNTU-CS
 
 int gcd(int a, int b) {
-    // поместить сюда текст реализации алгоритма (на С++)
+    int del = 0;
+	if(a == b){
+		return a;
+	}
+	else if(a < b){
+		if(0 == (b % a)){
+			return a;
+		}
+		else{
+			del = a;
+			for(; 0 < del; del--){
+				if(0 == (a%del) && 0 == (b%del)){
+					return del;
+					break;
+				}
+			}
+		}
+	}
+	else{
+		if(0 == (a % b)){
+			return b;
+		}
+		else{
+			int del = b;
+			for(; 0 < del; del--){
+				if(0 == (b%del) && 0 == (a%del)){
+					return del;
+					break;
+				}
+			}
+		}
+	}
+	if(0 == del){
+		return 1;
+	}
 }
